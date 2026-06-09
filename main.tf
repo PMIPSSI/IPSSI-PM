dd86e39634eb565f5fe48c5f4a560b5287fca9b5
PMIPSSI <p.monnier@ecole-ipssi.net>
2026-06-09 09:15:07 +0200

    Initial commit : TP01 S3 Secure Terraform

---
+++ main.tf
@@ -0,0 +1,68 @@
+data "aws_caller_identity" "current" {}
+
+resource "random_pet" "suffix" {
+  length    = 2
+  separator = "-"
+}
+
+# 1. Le bucket
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.bucket_prefix}-${data.aws_caller_identity.current.account_id}-${random_pet.suffix.id}"
+  tags = {
+    Owner = var.owner
+  }
+}
+
+# 2. Versioning
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# 3. Chiffrement SSE-S3
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# 4. Blocage accès public
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# 5. Policy TLS
+data "aws_iam_policy_document" "force_tls" {
+  statement {
+    sid     = "AllowSSLRequestsOnly"
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.main.arn,
+      "${aws_s3_bucket.main.arn}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.force_tls.json
+}
