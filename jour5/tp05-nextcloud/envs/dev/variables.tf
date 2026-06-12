81c55e0e602a281b1c7eb0a8c39af0a655e47348
PMIPSSI <p.monnier@ecole-ipssi.net>
2026-06-12 10:06:43 +0200

    Ajout TP05 Nextcloud Groupe2

---
+++ jour5/tp05-nextcloud/envs/dev/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" { type = string; default = "eu-west-3" }
+variable "allowed_admin_cidr" { type = string }
+variable "project_name" { type = string; default = "TP-PMONNIER" }
+variable "environment" { type = string; default = "dev" }
